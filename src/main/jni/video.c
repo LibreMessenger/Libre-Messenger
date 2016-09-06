2fb4c16f86f273775da4fcb828976d2565dea9a2
Christian Schneppe <kriztan@users.noreply.github.com>
2016-09-06 12:49:49 +0200

    set name correctly

--- src/main/jni/video.c
+++ src/main/jni/video.c
@@ -66,7 +66,7 @@ int isSemiPlanarYUV(int colorFormat) {
     }
 }
 
-JNIEXPORT int com_example_khalsa_video_MediaController_convertVideoFrame(JNIEnv *env, jclass class, jobject src, jobject dest, int destFormat, int width, int height, int padding, int swap) {
+JNIEXPORT int de_pixart_messenger_utils_video_MediaController_convertVideoFrame(JNIEnv *env, jclass class, jobject src, jobject dest, int destFormat, int width, int height, int padding, int swap) {
     if (!src || !dest || !destFormat) {
         return 0;
     }
