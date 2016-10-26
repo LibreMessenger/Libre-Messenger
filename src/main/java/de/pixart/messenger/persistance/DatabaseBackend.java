1052af9276e4c660b95b7b7fecfc8f40fe28c485
Christian Schneppe <christian@pix-art.de>
2016-10-26 22:13:24 +0200

    write prepped string to db. use display version everywhere else

--- src/main/java/de/pixart/messenger/persistance/DatabaseBackend.java
+++ src/main/java/de/pixart/messenger/persistance/DatabaseBackend.java
@@ -286,7 +286,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 					continue;
 				}
 				int ownDeviceId = Integer.valueOf(ownDeviceIdString);
-				AxolotlAddress ownAddress = new AxolotlAddress(account.getJid().toBareJid().toString(), ownDeviceId);
+				AxolotlAddress ownAddress = new AxolotlAddress(account.getJid().toBareJid().toPreppedString(), ownDeviceId);
 				deleteSession(db, account, ownAddress);
 				IdentityKeyPair identityKeyPair = loadOwnIdentityKeyPair(db, account);
 				if (identityKeyPair != null) {
@@ -344,7 +344,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 			try {
 				newJid = Jid.fromString(
 						cursor.getString(cursor.getColumnIndex(Conversation.CONTACTJID))
-				).toString();
+				).toPreppedString();
 			} catch (InvalidJidException ignored) {
 				Log.e(Config.LOGTAG, "Failed to migrate Conversation CONTACTJID "
 						+ cursor.getString(cursor.getColumnIndex(Conversation.CONTACTJID))
@@ -369,7 +369,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 			try {
 				newJid = Jid.fromString(
 						cursor.getString(cursor.getColumnIndex(Contact.JID))
-				).toString();
+				).toPreppedString();
 			} catch (InvalidJidException ignored) {
 				Log.e(Config.LOGTAG, "Failed to migrate Contact JID "
 						+ cursor.getString(cursor.getColumnIndex(Contact.JID))
@@ -577,8 +577,8 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 	public Conversation findConversation(final Account account, final Jid contactJid) {
 		SQLiteDatabase db = this.getReadableDatabase();
 		String[] selectionArgs = {account.getUuid(),
-				contactJid.toBareJid().toString() + "/%",
-				contactJid.toBareJid().toString()
+				contactJid.toBareJid().toPreppedString() + "/%",
+				contactJid.toBareJid().toPreppedString()
 		};
 		Cursor cursor = db.query(Conversation.TABLENAME, null,
 				Conversation.ACCOUNT + "=? AND (" + Conversation.CONTACTJID
@@ -690,7 +690,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 				db.insert(Contact.TABLENAME, null, contact.getContentValues());
 			} else {
 				String where = Contact.ACCOUNT + "=? AND " + Contact.JID + "=?";
-				String[] whereArgs = {account.getUuid(), contact.getJid().toString()};
+				String[] whereArgs = {account.getUuid(), contact.getJid().toPreppedString()};
 				db.delete(Contact.TABLENAME, where, whereArgs);
 			}
 		}
@@ -1024,7 +1024,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 	}
 
 	private IdentityKeyPair loadOwnIdentityKeyPair(SQLiteDatabase db, Account account) {
-		String name = account.getJid().toBareJid().toString();
+		String name = account.getJid().toBareJid().toPreppedString();
 		IdentityKeyPair identityKeyPair = null;
 		Cursor cursor = getIdentityKeyCursor(db, account, name, true);
 		if (cursor.getCount() != 0) {
@@ -1180,7 +1180,7 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 	}
 
 	public void storeOwnIdentityKeyPair(Account account, IdentityKeyPair identityKeyPair) {
-		storeIdentityKey(account, account.getJid().toBareJid().toString(), true, identityKeyPair.getPublicKey().getFingerprint().replaceAll("\\s", ""), Base64.encodeToString(identityKeyPair.serialize(), Base64.DEFAULT), XmppAxolotlSession.Trust.TRUSTED);
+		storeIdentityKey(account, account.getJid().toBareJid().toPreppedString(), true, identityKeyPair.getPublicKey().getFingerprint().replaceAll("\\s", ""), Base64.encodeToString(identityKeyPair.serialize(), Base64.DEFAULT), XmppAxolotlSession.Trust.TRUSTED);
 	}
 
 	public void recreateAxolotlDb(SQLiteDatabase db) {
