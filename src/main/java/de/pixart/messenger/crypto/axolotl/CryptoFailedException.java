4b8c7dc6304db7cd90d23ab80d85c1d534684ef0
Christian Schneppe <christian@pix-art.de>
2017-01-16 20:43:06 +0100

    partially improved logging for receiving omemo messages

--- src/main/java/de/pixart/messenger/crypto/axolotl/CryptoFailedException.java
+++ src/main/java/de/pixart/messenger/crypto/axolotl/CryptoFailedException.java
@@ -1,6 +1,11 @@
 package de.pixart.messenger.crypto.axolotl;
 
 public class CryptoFailedException extends Exception {
+
+    public CryptoFailedException(String msg) {
+        super(msg);
+    }
+
     public CryptoFailedException(Exception e) {
         super(e);
     }
