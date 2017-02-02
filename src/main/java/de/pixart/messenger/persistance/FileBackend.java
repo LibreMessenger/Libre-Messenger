22ccfc3092f6c5a4cdfff74f15672df9189ba244
Christian Schneppe <christian@pix-art.de>
2017-02-02 22:08:58 +0100

    fix detection of filesize

--- src/main/java/de/pixart/messenger/persistance/FileBackend.java
+++ src/main/java/de/pixart/messenger/persistance/FileBackend.java
@@ -148,9 +148,15 @@ public class FileBackend {
     private static long getFileSize(Context context, Uri uri) {
         Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
         if (cursor != null && cursor.moveToFirst()) {
-            return cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE));
+            long size = cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE));
+            cursor.close();
+            return size;
         } else {
-            return -1;
+            if (cursor != null) {
+                cursor.close();
+            }
+            File f = new File(uri.getPath());
+            return f.length();
         }
     }
 
