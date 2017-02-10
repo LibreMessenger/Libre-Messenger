53b200f6b17d134c8efbe19ef98d427548b46589
Christian Schneppe <christian@pix-art.de>
2017-02-10 21:03:10 +0100

    only show and try resend for files which are not uploaded

--- src/main/java/de/pixart/messenger/entities/Conversation.java
+++ src/main/java/de/pixart/messenger/entities/Conversation.java
@@ -168,7 +168,7 @@ public class Conversation extends AbstractEntity implements Blockable, Comparabl
             for (final Message message : this.messages) {
                 if ((message.getType() == Message.TYPE_IMAGE || message.getType() == Message.TYPE_FILE)
                         && message.getEncryption() != Message.ENCRYPTION_PGP) {
-                    if (message.getStatus() == Message.STATUS_SEND_FAILED && !message.isDeleted()) {
+                    if (message.getStatus() == Message.STATUS_SEND_FAILED && !message.isDeleted() && message.needsUploading()) {
                         onMessageFound.onMessageFound(message);
                     }
                 }
@@ -1016,7 +1016,7 @@ public class Conversation extends AbstractEntity implements Blockable, Comparabl
             for (int i = this.messages.size() - 1; i >= 0; --i) {
                 Message message = this.messages.get(i);
                 if ((message.getType() == Message.TYPE_IMAGE || message.getType() == Message.TYPE_FILE) && message.getEncryption() != Message.ENCRYPTION_PGP) {
-                    if (message.getStatus() == Message.STATUS_SEND_FAILED && !message.isDeleted()){
+                    if (message.getStatus() == Message.STATUS_SEND_FAILED && !message.isDeleted() && message.needsUploading()){
                         ++count;
                     }
                 }
