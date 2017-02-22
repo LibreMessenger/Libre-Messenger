0ce13fa8613e7df1215ef4582a14bcf8a3a77e96
Christian Schneppe <christian@pix-art.de>
2017-02-22 20:24:42 +0100

    Fix a typo in legacy

--- src/main/java/de/pixart/messenger/xmpp/XmppConnection.java
+++ src/main/java/de/pixart/messenger/xmpp/XmppConnection.java
@@ -1667,12 +1667,12 @@ public class XmppConnection implements Runnable {
 
         public boolean mam() {
             return hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM)
-                    || hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM_LAGECY);
+                    || hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM_LEGACY);
         }
 
         public boolean mamLegacy() {
             return !hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM)
-                    && hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM_LAGECY);
+                    && hasDiscoFeature(account.getJid().toBareJid(), Xmlns.MAM_LEGACY);
         }
 
         public boolean push() {
