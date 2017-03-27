967c36b5e21fda4caf9ec44ba74319e226adb064
Christian Schneppe <kriztan@users.noreply.github.com>
2017-03-27 12:56:19 +0200

    Update FILE_LINK.php

--- .github/FILE_LINK.php
+++ .github/FILE_LINK.php
@@ -1,5 +1,10 @@
 <?php
 $json = file_get_contents('https://api.github.com/repos/kriztan/Pix-Art-Messenger/releases/latest');
 $data = json_decode($json);
-echo $data->assets[0]->browser_download_url;
+$filename = $data->assets[0]->name;
+$filesize = $data->assets[0]->size;
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.$filename.'";');
+header('Content-Length: '.$filesize);
+readfile($data->assets[0]->browser_download_url);
 ?>
