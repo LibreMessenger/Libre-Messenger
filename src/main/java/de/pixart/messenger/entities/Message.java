acc23261c687d7a66d772b3a92c69002e7001b94
Christian Schneppe <christian@pix-art.de>
2017-04-05 20:41:28 +0200

    do not parse bodies with http urls as downloadable in omemo encrypted messages

--- src/main/java/de/pixart/messenger/entities/Message.java
+++ src/main/java/de/pixart/messenger/entities/Message.java
@@ -676,16 +676,14 @@ public class Message extends AbstractEntity {
             }
             if (!url.getProtocol().equalsIgnoreCase("http") && !url.getProtocol().equalsIgnoreCase("https")) {
                 return Decision.NEVER;
-            } else if (oob) {
+            } else if (oob || encrypted) {
                 return Decision.MUST;
             }
-            String extension = extractRelevantExtension(url);
-            if (extension == null) {
+            final String extension = extractRelevantExtension(url);
+            if (extension == null
+                    || encryption == Message.ENCRYPTION_OTR
+                    || encryption == Message.ENCRYPTION_AXOLOTL) {
                 return Decision.NEVER;
-            }
-
-            if (encrypted) {
-                return Decision.MUST;
             } else if (Transferable.VALID_IMAGE_EXTENSIONS.contains(extension)
                     || Transferable.WELL_KNOWN_EXTENSIONS.contains(extension)) {
                 return Decision.SHOULD;
