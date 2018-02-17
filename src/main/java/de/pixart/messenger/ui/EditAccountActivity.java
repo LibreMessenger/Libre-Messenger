26bb133b103ee393218316bcabbea4431b96708e
Christian Schneppe <christian@pix-art.de>
2018-02-17 21:12:40 +0100

    Don't add account if user cancels

--- src/main/java/de/pixart/messenger/ui/EditAccountActivity.java
+++ src/main/java/de/pixart/messenger/ui/EditAccountActivity.java
@@ -278,7 +278,7 @@ public class EditAccountActivity extends OmemoActivity implements OnAccountUpdat
 
         @Override
         public void onClick(final View v) {
-            deleteMagicCreatedAccountAndReturnIfNecessary();
+            deleteAccountAndReturnIfNecessary();
             finish();
         }
     };
@@ -311,22 +311,21 @@ public class EditAccountActivity extends OmemoActivity implements OnAccountUpdat
 
     @Override
     public boolean onNavigateUp() {
-        deleteMagicCreatedAccountAndReturnIfNecessary();
+        deleteAccountAndReturnIfNecessary();
         return super.onNavigateUp();
     }
 
     @Override
     public void onBackPressed() {
-        deleteMagicCreatedAccountAndReturnIfNecessary();
+        deleteAccountAndReturnIfNecessary();
         super.onBackPressed();
     }
 
-    private void deleteMagicCreatedAccountAndReturnIfNecessary() {
-        if (Config.MAGIC_CREATE_DOMAIN != null
-                && mAccount != null
-                && mAccount.isOptionSet(Account.OPTION_MAGIC_CREATE)
-                && mAccount.isOptionSet(Account.OPTION_REGISTER)
-                && xmppConnectionService.getAccounts().size() == 1) {
+    private void deleteAccountAndReturnIfNecessary() {
+        if (mInitMode && mAccount != null && !mAccount.isOptionSet(Account.OPTION_LOGGED_IN_SUCCESSFULLY)) {
+            xmppConnectionService.deleteAccount(mAccount);
+        }
+        if (xmppConnectionService.getAccounts().size() == 0) {
             xmppConnectionService.deleteAccount(mAccount);
             Intent intent = new Intent(EditAccountActivity.this, WelcomeActivity.class);
             WelcomeActivity.addInviteUri(intent, getIntent());
