84d4869572f69014793b463e6613230dc39b5370
Christian Schneppe <christian@pix-art.de>
2018-03-21 19:36:19 +0100

    fix crashes because requestCode was too long

--- src/main/java/de/pixart/messenger/ui/XmppActivity.java
+++ src/main/java/de/pixart/messenger/ui/XmppActivity.java
@@ -88,8 +88,8 @@ public abstract class XmppActivity extends AppCompatActivity {
     protected static final int REQUEST_ANNOUNCE_PGP = 0x0101;
     protected static final int REQUEST_INVITE_TO_CONVERSATION = 0x0102;
     protected static final int REQUEST_CHOOSE_PGP_ID = 0x0103;
-    protected static final int REQUEST_BATTERY_OP = 0x13849ff;
-    protected static final int REQUEST_UNKNOWN_SOURCE_OP = 0x14958ff;
+    protected static final int REQUEST_BATTERY_OP = 0x49ff;
+    protected static final int REQUEST_UNKNOWN_SOURCE_OP = 0x98ff;
 
     public static final String EXTRA_ACCOUNT = "account";
 
