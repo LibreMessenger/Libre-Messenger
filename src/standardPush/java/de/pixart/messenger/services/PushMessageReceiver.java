ac3926cfbf29c6e1ca62a66f318efad666d17a19
Christian Schneppe <christian@pix-art.de>
2018-05-21 20:55:52 +0200

    make fcm receiver ignore messages when all accounts are disabled

--- src/standardPush/java/de/pixart/messenger/services/PushMessageReceiver.java
+++ src/standardPush/java/de/pixart/messenger/services/PushMessageReceiver.java
@@ -1,13 +1,20 @@
 package de.pixart.messenger.services;
 
 import android.content.Intent;
+import android.util.Log;
 
 import java.util.Map;
+
+import de.pixart.messenger.Config;
 
 public class PushMessageReceiver extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
+        if (!EventReceiver.hasEnabledAccounts(this)) {
+            Log.d(Config.LOGTAG, "PushMessageReceiver ignored message because no accounts are enabled");
+            return;
+        }
         Map<String, String> data = message.getData();
         Intent intent = new Intent(this, XmppConnectionService.class);
         intent.setAction(XmppConnectionService.ACTION_FCM_MESSAGE_RECEIVED);
