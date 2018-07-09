e4a9cecddab5fd53d30c49fe759788d2a5f74ad0
Christian Schneppe <christian@pix-art.de>
2018-07-09 22:11:55 +0200

    make xmppuri deal with null input

--- src/main/java/de/pixart/messenger/utils/XmppUri.java
+++ src/main/java/de/pixart/messenger/utils/XmppUri.java
@@ -57,7 +57,10 @@ public class XmppUri {
         return safeSource;
     }
 
-    protected void parse(Uri uri) {
+    protected void parse(final Uri uri) {
+        if (uri == null) {
+            return;
+        }
         this.uri = uri;
         String scheme = uri.getScheme();
         String host = uri.getHost();
