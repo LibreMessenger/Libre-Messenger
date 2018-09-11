5608bbf94d94ca50a0bc2d79e196b1eb09f9b0b4
Christian Schneppe <christian@pix-art.de>
2018-09-11 21:15:22 +0200

    catch illegal state not illegal argument on popBackStack()

--- src/main/java/de/pixart/messenger/ui/ConversationsActivity.java
+++ src/main/java/de/pixart/messenger/ui/ConversationsActivity.java
@@ -548,7 +548,7 @@ public class ConversationsActivity extends XmppActivity implements OnConversatio
                 if (fm.getBackStackEntryCount() > 0) {
                     try {
                         fm.popBackStack();
-                    } catch (IllegalArgumentException e) {
+                    } catch (IllegalStateException e) {
                         Log.w(Config.LOGTAG, "Unable to pop back stack after pressing home button");
                     }
                     return true;
