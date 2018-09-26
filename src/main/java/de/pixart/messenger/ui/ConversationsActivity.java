dd7bcdfbef276a0b18bf3d4cfee8b8a51b80a947
Christian Schneppe <christian@pix-art.de>
2018-09-26 21:16:55 +0200

    clear parent activities pending view intent before calling startAcitvityForResult()

--- src/main/java/de/pixart/messenger/ui/ConversationsActivity.java
+++ src/main/java/de/pixart/messenger/ui/ConversationsActivity.java
@@ -490,11 +490,18 @@ public class ConversationsActivity extends XmppActivity implements OnConversatio
 
     @Override
     public void onConversationSelected(Conversation conversation) {
+        clearPendingViewIntent();
         if (ConversationFragment.getConversation(this) == conversation) {
             Log.d(Config.LOGTAG, "ignore onConversationSelected() because conversation is already open");
             return;
         }
         openConversation(conversation, null);
+    }
+
+    public void clearPendingViewIntent() {
+        if (pendingViewIntent.clear()) {
+            Log.e(Config.LOGTAG, "cleared pending view intent");
+        }
     }
 
     private void displayToast(final String msg) {
