f0e96ca60df9440b30fd5107160786e72a27f617
Christian Schneppe <christian@pix-art.de>
2018-11-08 22:24:44 +0100

    try to catch exceptions on FCM token refresh

--- src/standardPush/java/de/pixart/messenger/services/InstanceIdService.java
+++ src/standardPush/java/de/pixart/messenger/services/InstanceIdService.java
@@ -1,8 +1,13 @@
 package de.pixart.messenger.services;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import de.pixart.messenger.Config;
+import de.pixart.messenger.utils.Compatibility;
 
 public class InstanceIdService extends FirebaseInstanceIdService {
 
@@ -10,6 +15,10 @@ public class InstanceIdService extends FirebaseInstanceIdService {
     public void onTokenRefresh() {
         Intent intent = new Intent(this, XmppConnectionService.class);
         intent.setAction(XmppConnectionService.ACTION_FCM_TOKEN_REFRESH);
-        startService(intent);
+        try {
+            startService(intent);
+        } catch (Exception e) {
+            Log.e(Config.LOGTAG, "unable to refresh FCM token", e);
+        }
     }
 }
