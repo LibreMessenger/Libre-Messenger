ef779d5c2e4e5c63160e7da85c90ddb5a165ebbd
Christian Schneppe <christian@pix-art.de>
2018-11-23 13:04:45 +0100

    suggest sync on login

--- src/main/java/de/pixart/messenger/services/XmppConnectionService.java
+++ src/main/java/de/pixart/messenger/services/XmppConnectionService.java
@@ -307,6 +307,7 @@ public class XmppConnectionService extends Service {
             }
             account.getRoster().clearPresences();
             mJingleConnectionManager.cancelInTransmission();
+            mQuickConversationsService.considerSync();
             fetchRosterFromServer(account);
             if (!account.getXmppConnection().getFeatures().bookmarksConversion()) {
                 fetchBookmarks(account);
