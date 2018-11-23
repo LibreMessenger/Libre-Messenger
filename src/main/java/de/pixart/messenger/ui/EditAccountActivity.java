b84275d10791c10c6a986c87eae164b9f2e0c1b9
Christian Schneppe <christian@pix-art.de>
2018-11-23 15:00:42 +0100

    cancel magic mode attribute after changing password once

--- src/main/java/de/pixart/messenger/ui/EditAccountActivity.java
+++ src/main/java/de/pixart/messenger/ui/EditAccountActivity.java
@@ -203,7 +203,7 @@ public class EditAccountActivity extends OmemoActivity implements OnAccountUpdat
                 }
             }
             if (mAccount != null) {
-                if (mInitMode && mAccount.isOptionSet(Account.OPTION_MAGIC_CREATE)) {
+                if (mAccount.isOptionSet(Account.OPTION_MAGIC_CREATE)) {
                     mAccount.setOption(Account.OPTION_MAGIC_CREATE, mAccount.getPassword().contains(password));
                 }
                 mAccount.setJid(jid);
