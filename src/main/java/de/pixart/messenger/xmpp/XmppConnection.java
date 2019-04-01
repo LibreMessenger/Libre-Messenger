69af6c85c785cf7329033de4da129c19bc864134
Christian Schneppe <christian@pix-art.de>
2019-04-01 21:35:13 +0200

    remove logging from delete from server

--- src/main/java/de/pixart/messenger/xmpp/XmppConnection.java
+++ src/main/java/de/pixart/messenger/xmpp/XmppConnection.java
@@ -1022,7 +1022,6 @@ public class XmppConnection implements Runnable {
         delete.query("jabber:iq:register").addChild("remove");
         delete.setTo(Jid.of(account.getServer()));
         delete.setFrom(account.getJid().asBareJid());
-        Log.d(Config.LOGTAG, "Delete " + delete);
         sendUnmodifiedIqPacket(delete, (account, packet) -> {
             if (packet.getType() == IqPacket.TYPE.TIMEOUT) {
                 return;
