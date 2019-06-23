ce16d024a66a889a64d278533762759c21b6c26c
Christian Schneppe <christian@pix-art.de>
2019-06-23 09:31:51 +0200

    Revert "BackupExport format like Conversations (#342)"
    This patch is not compatible with Android SDK < 26 (Android 8 / OREO)
    This reverts commit 514b63f48684ab82031b0291da8d303abe1d680c.

--- src/main/java/de/pixart/messenger/services/ExportBackupService.java
+++ src/main/java/de/pixart/messenger/services/ExportBackupService.java
@@ -261,15 +261,7 @@ public class ExportBackupService extends Service {
     }
 
     private void messageExport(SQLiteDatabase db, String uuid, PrintWriter writer, Progress progress) {
-        // not select and create column Message.FILE_DELETED to be compareable with conversations
-        Cursor cursor = db.rawQuery("select messages." + String.join(", messages.", new String[] {
-                Message.UUID, Message.CONVERSATION, Message.TIME_SENT, Message.COUNTERPART, Message.TRUE_COUNTERPART,
-                Message.BODY, Message.ENCRYPTION, Message.STATUS, Message.TYPE, Message.RELATIVE_FILE_PATH,
-                Message.SERVER_MSG_ID, Message.FINGERPRINT, Message.CARBON, Message.EDITED, Message.READ,
-                Message.DELETED, Message.OOB, Message.ERROR_MESSAGE, Message.READ_BY_MARKERS, Message.MARKABLE,
-                Message.REMOTE_MSG_ID, Message.CONVERSATION
-            }) + " from messages join conversations on conversations.uuid=messages.conversationUuid where conversations.accountUuid=?", new String[]{uuid});
-
+        Cursor cursor = db.rawQuery("select messages.* from messages join conversations on conversations.uuid=messages.conversationUuid where conversations.accountUuid=?", new String[]{uuid});
         int size = cursor != null ? cursor.getCount() : 0;
         Log.d(Config.LOGTAG, "exporting " + size + " messages");
         int i = 0;
