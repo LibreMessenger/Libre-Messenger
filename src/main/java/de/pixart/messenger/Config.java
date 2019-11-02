d69583c3f3156aca438a023062632d9d6f6ecc7f
Christian Schneppe <christian.schneppe@pix-art.de>
2019-11-02 16:07:46 +0100

    move some more config variables to match different app modes

--- src/main/java/de/pixart/messenger/Config.java
+++ src/main/java/de/pixart/messenger/Config.java
@@ -17,7 +17,7 @@ public final class Config {
     private static final int OTR = 4;
     private static final int OMEMO = 8;
 
-    private static final int ENCRYPTION_MASK = UNENCRYPTED | OPENPGP | OTR | OMEMO;
+    private static final int ENCRYPTION_MASK = BuildConfig.ENCRYPTION_MASK;
 
     public static boolean supportUnencrypted() {
         return (ENCRYPTION_MASK & UNENCRYPTED) != 0;
@@ -50,8 +50,8 @@ public final class Config {
 
     public static final String XMPP_IP = null; //BuildConfig.XMPP_IP; // set to null means disable
     public static final Integer[] XMPP_Ports = null; //BuildConfig.XMPP_Ports; // set to null means disable
-    public static final String DOMAIN_LOCK = null; //BuildConfig.DOMAIN_LOCK; //only allow account creation for this domain
-    public static final String MAGIC_CREATE_DOMAIN = "blabber.im";
+    public static final String DOMAIN_LOCK = BuildConfig.DOMAIN_LOCK; //only allow account creation for this domain
+    public static final String MAGIC_CREATE_DOMAIN = BuildConfig.MAGIC_CREATE_DOMAIN; //"blabber.im";
     public static final String QUICKSY_DOMAIN = "quicksy.im";
     public static final String CHANNEL_DISCOVERY = "https://search.jabber.network";
     public static final String DEFAULT_INVIDIOUS_HOST = "invidio.us";
