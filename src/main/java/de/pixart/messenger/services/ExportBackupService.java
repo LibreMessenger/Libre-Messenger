da925991c10da1cd763442c1e8b214994e926532
Christian Schneppe <christian.schneppe@pix-art.de>
2019-11-16 11:27:30 +0100

    make exporting backup compatible with conversations

--- src/main/java/de/pixart/messenger/services/ExportBackupService.java
+++ src/main/java/de/pixart/messenger/services/ExportBackupService.java
@@ -280,11 +280,12 @@ public class ExportBackupService extends Service {
         Cursor cursor;
         if (runsTwentySix()) {
             // not select and create column Message.FILE_DELETED to be compareable with conversations
+            // in C Message.DELETED = Message.FILE_DELETED in PAM so do not select this column, too.
             cursor = db.rawQuery("select messages." + String.join(", messages.", new String[]{
                     Message.UUID, Message.CONVERSATION, Message.TIME_SENT, Message.COUNTERPART, Message.TRUE_COUNTERPART,
                     Message.BODY, Message.ENCRYPTION, Message.STATUS, Message.TYPE, Message.RELATIVE_FILE_PATH,
                     Message.SERVER_MSG_ID, Message.FINGERPRINT, Message.CARBON, Message.EDITED, Message.READ,
-                    Message.DELETED, Message.OOB, Message.ERROR_MESSAGE, Message.READ_BY_MARKERS, Message.MARKABLE,
+                    Message.OOB, Message.ERROR_MESSAGE, Message.READ_BY_MARKERS, Message.MARKABLE,
                     Message.REMOTE_MSG_ID, Message.CONVERSATION
             }) + " from messages join conversations on conversations.uuid=messages.conversationUuid where conversations.accountUuid=?", new String[]{uuid});
         } else {
