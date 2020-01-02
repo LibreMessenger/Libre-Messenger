a5f52ef599ae5519bd704fae8ceb1dd78c7edcc6
Christian Schneppe <christian.schneppe@pix-art.de>
2020-01-02 09:05:49 +0100

    make parts in Message.java final

--- src/main/java/de/pixart/messenger/entities/Message.java
+++ src/main/java/de/pixart/messenger/entities/Message.java
@@ -843,7 +843,7 @@ public class Message extends AbstractEntity implements AvatarService.Avatarable 
             if (this.transferable != null) {
                 fileParams.size = this.transferable.getFileSize();
             }
-            String[] parts = body == null ? new String[0] : body.split("\\|");
+            final String[] parts = body == null ? new String[0] : body.split("\\|");
             switch (parts.length) {
                 case 1:
                     try {
